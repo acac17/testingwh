dcffb108ea08e9c495f626107568ded95c7111a9
AC <anshumanchadda@gmail.com>
2021-08-07 21:48:23 -0700

    small change

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def post_something():
 # A welcome message to test our server
 @app.route('/')
 def index():
-    return "<h1>Welcome to our Webhook Server from VSCODE noww Push!!</h1>"
+    return "<h1>Anshuman Chadda</h1>"
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
@@ -57,3 +57,4 @@ if __name__ == '__main__':
     # replitToHeroku
     #checkingchanges
     #checkingchangesextra
+    #makechangessome
